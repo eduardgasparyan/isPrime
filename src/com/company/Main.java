4fb793d832a7b747a4febe3fe182df27bb5b7b16
eduardgasparyan <44871039+eduardgasparyan@users.noreply.github.com>
2020-08-22 12:41:26 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-int [] mass = new int[100];
-for (int i=0;i<100;i++)
-    {
-        mass[i] = (int) (Math.random() * 1000);
-    }
-for (int i=0; i < mass.length; i++)
-{
-    for (int j=0;j<mass.length-i-1;j++)
-    {
-        if  (mass[j] > mass[j+1])
-        {
-            int test = mass[j];
-            mass[j] = mass[j+1];
-            mass[j+1] = test;
+for(int i = 2; i <= 100; i ++){
+            boolean isPrime = true;
+
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime){
+                System.out.println(i);
+            }
         }
-    }
-}
-for (int i=0;i<mass.length;i++)
-{
-    System.out.println(mass[i]);
-}
     }
 }
